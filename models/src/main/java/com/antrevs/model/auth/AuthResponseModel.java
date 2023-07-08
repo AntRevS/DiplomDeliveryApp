5faf04985536bdc12c147dc4ebd40571e8657b25
Anton Reviakin <reviakin.space@gmail.com>
2023-07-08 14:39:26 +0400

    update auth models

--- models/src/main/java/com/antrevs/model/auth/AuthResponseModel.java
+++ models/src/main/java/com/antrevs/model/auth/AuthResponseModel.java
@@ -7,11 +7,21 @@ public class AuthResponseModel extends BaseResponseModel {
 
     private UserModel user;
 
+    private String sessionId;
+
     public UserModel getUser() {
         return user;
     }
 
     public void setUser(UserModel user) {
         this.user = user;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
     }
 }
