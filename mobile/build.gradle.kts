ce0c7bd1eea00fced45520af3f3d86b44f7c8eb9
Anton Reviakin <reviakin.space@gmail.com>
2023-09-17 03:26:32 +0400

    Update detekt logic

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,18 +1,39 @@
+import java.io.ByteArrayOutputStream
+
 plugins {
     id("io.gitlab.arturbosch.detekt") version("1.23.1")
 }
 
 detekt {
-    toolVersion = "1.23.1"
+    toolVersion = AppVersions.DETEKT
     config.setFrom(file("detekt-config.yml"))
-    val moduleNames = listOf("core", "feature", "app")
-    val listOfModules = projectDir.absoluteFile.listFiles()?.filter {
-        it.isDirectory && moduleNames.any { name ->
-            it.name.contains(name, ignoreCase = true)
-        }
-    }?.map { "${it.name}/src/main/java" }
-    source.setFrom(listOfModules)
+    val listOfFiles = getDiffFilesList()
+    printGreen("Files will be detekt:")
+    listOfFiles.forEach(::printBlue)
+    source.setFrom(listOfFiles)
     basePath = projectDir.path
+}
+
+fun printGreen(text: String) = println("\u001b[32m$text\u001b[0m")
+
+fun printBlue(text: String) = println("\u001b[34m$text\u001b[0m")
+
+fun getDiffFilesList(): List<String> =
+    runCommand("git diff --name-only main")
+        .split("\n")
+        .filter { it.startsWith("mobile") }
+        .map { it.replace("mobile/", "") }
+
+fun runCommand(command: String): String {
+    val outputStream = ByteArrayOutputStream()
+    project.exec {
+        commandLine = command.split(" ")
+        standardOutput = outputStream
+    }
+    String(outputStream.toByteArray()).let { result ->
+        //println("run command result: $result")
+        return result.trim()
+    }
 }
 
 buildscript {
@@ -22,6 +43,6 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
-        classpath("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
+        classpath(AppDependencies.DETEKT_FORMATTING)
     }
 }
