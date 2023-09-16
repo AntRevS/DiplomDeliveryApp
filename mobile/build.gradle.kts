c895b9517a18c851f733b726c9c04425016a210c
Anton Reviakin <reviakin.space@gmail.com>
2023-09-16 09:35:31 +0400

    add detekt configuration

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,3 +1,20 @@
+plugins {
+    id("io.gitlab.arturbosch.detekt") version("1.23.1")
+}
+
+detekt {
+    toolVersion = "1.23.1"
+    config.setFrom(file("detekt-config.yml"))
+    val moduleNames = listOf("core", "feature", "app")
+    val listOfModules = projectDir.absoluteFile.listFiles()?.filter {
+        it.isDirectory && moduleNames.any { name ->
+            it.name.contains(name, ignoreCase = true)
+        }
+    }?.map { "${it.name}/src/main/java" }
+    source.setFrom(listOfModules)
+    basePath = projectDir.path
+}
+
 buildscript {
     repositories {
         google()
@@ -5,5 +22,6 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
+        classpath("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.1")
     }
 }
